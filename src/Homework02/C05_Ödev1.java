7d47b1a85517dac1c150cac7fe2cb69a2f935886
samet ozcelik <smtab142790@gmail.com>
2023-03-29 15:00:46 +0300

    19/03/2023

---
+++ src/Homework02/C05_Ödev1.java
@@ -0,0 +1,22 @@
+package Homework02;
+
+public class C05_Ödev1  {
+    public static void main(String[] args) {
+
+        /*
+        4- Soru: Double 255.36 sayısını int'a ve sonra da olusturduğunuz
+        int sayıyı byte'a cevirip yazdirin.
+         */
+
+        double sayi1 = 255.36;
+
+        int sayi2 = (int) sayi1;
+
+        byte sayi3 = (byte) sayi2;
+
+        System.out.println("sayi3 = " + sayi3);
+
+
+
+    }
+}
